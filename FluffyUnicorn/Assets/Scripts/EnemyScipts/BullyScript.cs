73d82fe0b87730dba1e17d7678d96dda878586c1
Adlez <srgnt_booyaw@live.ca>
2015-04-21 11:18:09 -0400

    Relocated Enemy Constants
    moved them to Constants and regioned them

--- FluffyUnicorn/Assets/Scripts/EnemyScipts/BullyScript.cs
+++ FluffyUnicorn/Assets/Scripts/EnemyScipts/BullyScript.cs
@@ -11,38 +11,38 @@ public class BullyScript : EnemyBaseClass
 	{
 		base.InitEnemy();
 
-		m_VelocityX = EnemyConstValues.BULLY_VEL_X;
-		m_AttackTimer = EnemyConstValues.BULLY_ATTACK_TIMER_RESET_VALUE;
-		m_HP = EnemyConstValues.BULLY_HP;
+		m_VelocityX = Constants.BULLY_VEL_X;
+		m_AttackTimer = Constants.BULLY_ATTACK_TIMER_RESET_VALUE;
+		m_HP = Constants.BULLY_HP;
 		m_Damage = 0;
 		//		m_CurRow = Random.Range(1, 3);
 		m_EnemyInMotion = true;
 
-		m_AttackPunchOdds = EnemyConstValues.BULLY_PUNCH_ODDS;
-		m_AttackKickOdds = EnemyConstValues.BULLY_KICK_ODDS;
-		m_AttackUniqueOdds = EnemyConstValues.BULLY_UNIQUE_ATK_ODDS;
+		m_AttackPunchOdds = Constants.BULLY_PUNCH_ODDS;
+		m_AttackKickOdds = Constants.BULLY_KICK_ODDS;
+		m_AttackUniqueOdds = Constants.BULLY_UNIQUE_ATK_ODDS;
 	}
 	public override void EnemyAttackKick()
 	{
 		//play Bully's Kick Animation
-		m_Damage = EnemyConstValues.BULLY_KICK_DAMAGE;
-		float AttackTimer = EnemyConstValues.BULLY_ATTACK_TIMER_RESET_VALUE + EnemyConstValues.BULLY_KICK_RESTTIME;
+		m_Damage = Constants.BULLY_KICK_DAMAGE;
+		float AttackTimer = Constants.BULLY_ATTACK_TIMER_RESET_VALUE + Constants.BULLY_KICK_RESTTIME;
 		ResetEnemyAttackTimer(AttackTimer);
 
 	}
 	public override void EnemyAttackPunch()
 	{
 		//play Bully's Kick Animation
-		m_Damage = EnemyConstValues.BULLY_PUNCH_DAMAGE;
-		float AttackTimer = EnemyConstValues.BULLY_ATTACK_TIMER_RESET_VALUE + EnemyConstValues.BULLY_PUNCH_RESTTIME;
+		m_Damage = Constants.BULLY_PUNCH_DAMAGE;
+		float AttackTimer = Constants.BULLY_ATTACK_TIMER_RESET_VALUE + Constants.BULLY_PUNCH_RESTTIME;
           ResetEnemyAttackTimer(AttackTimer);
 
 	}
 	public override void EnemyAttackUnique()
 	{
 		//play Bully's Kick Animation
-		m_Damage = EnemyConstValues.BULLY_UNIQUE_ATTACK_DAMAGE;
-		float AttackTimer = EnemyConstValues.BULLY_ATTACK_TIMER_RESET_VALUE + EnemyConstValues.BULLY_UNIQUE_ATK_RESTTIME;
+		m_Damage = Constants.BULLY_UNIQUE_ATTACK_DAMAGE;
+		float AttackTimer = Constants.BULLY_ATTACK_TIMER_RESET_VALUE + Constants.BULLY_UNIQUE_ATK_RESTTIME;
           ResetEnemyAttackTimer(AttackTimer);
 	}
 
--- FluffyUnicorn/Assets/Scripts/EnemyScipts/EnemyConstValues.cs
+++ FluffyUnicorn/Assets/Scripts/EnemyScipts/EnemyConstValues.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class EnemyConstValues : MonoBehaviour
 {
-	//Regular Bully Values
+	#region bully variables
 	public const float BULLY_ATTACK_TIMER_RESET_VALUE = 5;
 	public const int BULLY_VEL_X = -2;
 
@@ -19,5 +19,77 @@ public class EnemyConstValues : MonoBehaviour
 	public const float BULLY_KICK_RESTTIME = 2;
 	public const float BULLY_PUNCH_RESTTIME = 1;
 	public const float BULLY_UNIQUE_ATK_RESTTIME = 3;
-	
+	#endregion
+
+	#region fat bully variables
+	public const float FAT_BULLY_ATTACK_TIMER_RESET_VALUE = 8;
+	public const int FAT_BULLY_VEL_X = -1;
+
+	public const int FAT_BULLY_HP = 5;
+	public const int FAT_BULLY_PUNCH_DAMAGE = 1;
+	public const int FAT_BULLY_KICK_DAMAGE = 1;
+	public const int FAT_BULLY_UNIQUE_ATTACK_DAMAGE = 6;
+
+	public const int FAT_BULLY_PUNCH_ODDS = 10;
+	public const int FAT_BULLY_KICK_ODDS = 25;
+	public const int FAT_BULLY_UNIQUE_ATK_ODDS = 100;
+
+	public const float FAT_BULLY_KICK_RESTTIME = 3;
+	public const float FAT_BULLY_PUNCH_RESTTIME = 3;
+	public const float FAT_BULLY_UNIQUE_ATK_RESTTIME = 6;
+	#endregion
+
+	#region jock bully variables
+	public const float JOCK_BULLY_ATTACK_TIMER_RESET_VALUE = 3;
+	public const int JOCK_BULLY_VEL_X = -3;
+
+	public const int JOCK_BULLY_HP = 4;
+	public const int JOCK_BULLY_PUNCH_DAMAGE = 3;
+	public const int JOCK_BULLY_KICK_DAMAGE = 3;
+	public const int JOCK_BULLY_UNIQUE_ATTACK_DAMAGE = 4;
+
+	public const int JOCK_BULLY_PUNCH_ODDS = 40;
+	public const int JOCK_BULLY_KICK_ODDS = 60;
+	public const int JOCK_BULLY_UNIQUE_ATK_ODDS = 100;
+
+	public const float JOCK_BULLY_KICK_RESTTIME = 1.5f;
+	public const float JOCK_BULLY_PUNCH_RESTTIME = 1;
+	public const float JOCK_BULLY_UNIQUE_ATK_RESTTIME = 3.5f;
+	#endregion
+
+	#region bling bully variables
+	public const float BLING_BULLY_ATTACK_TIMER_RESET_VALUE = 1;
+	public const int BLING_BULLY_VEL_X = -2;
+
+	public const int BLING_BULLY_HP = 3;
+	public const int BLING_BULLY_PUNCH_DAMAGE = 2;
+	public const int BLING_BULLY_KICK_DAMAGE = 2;
+	public const int BLING_BULLY_UNIQUE_ATTACK_DAMAGE = 5;
+
+	public const int BLING_BULLY_PUNCH_ODDS = 60;
+	public const int BLING_BULLY_KICK_ODDS = 80;
+	public const int BLING_BULLY_UNIQUE_ATK_ODDS = 100;
+
+	public const float BLING_BULLY_KICK_RESTTIME = 2;
+	public const float BLING_BULLY_PUNCH_RESTTIME = 1;
+	public const float BLING_BULLY_UNIQUE_ATK_RESTTIME = 3;
+	#endregion
+
+	#region pepper bully variables
+	public const float PEPPER_BULLY_ATTACK_TIMER_RESET_VALUE = 4;
+	public const int PEPPER_BULLY_VEL_X = -2;
+
+	public const int PEPPER_BULLY_HP = 3;
+	public const int PEPPER_BULLY_PUNCH_DAMAGE = 1;
+	public const int PEPPER_BULLY_KICK_DAMAGE = 2;
+	public const int PEPPER_BULLY_UNIQUE_ATTACK_DAMAGE = 4;
+
+	public const int PEPPER_BULLY_PUNCH_ODDS = 40;
+	public const int PEPPER_BULLY_KICK_ODDS = 70;
+	public const int PEPPER_BULLY_UNIQUE_ATK_ODDS = 100;
+
+	public const float PEPPER_BULLY_KICK_RESTTIME = 1;
+	public const float PEPPER_BULLY_PUNCH_RESTTIME = 0;
+	public const float PEPPER_BULLY_UNIQUE_ATK_RESTTIME = 2;
+	#endregion
 }
