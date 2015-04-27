1cbde672773ad87193f706bb0ec24299081a7411
AHBrandon <brandoncoulthard@hotmail.com>
2015-04-27 09:30:02 -0400

    Start of destrructable objects

---
+++ FluffyUnicorn/Assets/Scripts/Brandon's Scripts/DestructableObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestructableObject : MonoBehaviour 
+{
+    public float m_Health = 0;
+
+    private GameObject player_;
+
+    void Start()
+    {
+        player_ = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    public void Destroy(float damage)
+    {
+        m_Health -= damage;
+        if (m_Health <= 0)
+        {
+            ObjectPool.Instance.PoolObject(this.gameObject);
+        }
+    }
+}
--- FluffyUnicorn/Assets/Scripts/Brandon's Scripts/Projectile.cs
+++ FluffyUnicorn/Assets/Scripts/Brandon's Scripts/Projectile.cs
@@ -10,9 +10,15 @@ public class Projectile : MonoBehaviour {
     void OnTriggerEnter(Collider other)
     {
         m_Damage = player_.GetComponent<PlayerController>().m_PlayerDamage;
+        DestructableObject hitObject;
         if (other.tag == m_Enemy)
         {
             //do stuff
+        }
+        else if (other.tag == "TrashCan")
+        {
+            hitObject = other.gameObject.GetComponentInChildren<DestructableObject>();
+            hitObject.Destroy(m_Damage);
         }
     }
 }
