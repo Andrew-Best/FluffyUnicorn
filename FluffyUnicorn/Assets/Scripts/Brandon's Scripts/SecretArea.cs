e61b03c509e79633f5e06a6d375c592304dcc708
AHBrandon <brandoncoulthard@hotmail.com>
2015-04-29 09:47:01 -0400

    Start of secret areas

---
+++ FluffyUnicorn/Assets/Scripts/Brandon's Scripts/SecretArea.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecretArea : MonoBehaviour
+{
+    #region public
+ 
+    #endregion
+    private GameObject player_;
+    private GameObject enemySpawner_;
+    #region private
+
+    #endregion
+    // Use this for initialization
+	void Start ()
+    {
+        player_ = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
