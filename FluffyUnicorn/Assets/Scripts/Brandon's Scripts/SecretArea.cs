d136a276ffa4d5f5b16c03c01a18d35ede3c6aa3
AHBrandon <brandoncoulthard@hotmail.com>
2015-04-29 11:24:32 -0400

    More secret area code

--- FluffyUnicorn/Assets/Scripts/Brandon's Scripts/SecretArea.cs
+++ FluffyUnicorn/Assets/Scripts/Brandon's Scripts/SecretArea.cs
@@ -4,22 +4,44 @@ using System.Collections;
 public class SecretArea : MonoBehaviour
 {
     #region public
- 
+    public SpawnEnemies m_EnemySpawner;
     #endregion
-    private GameObject player_;
-    private GameObject enemySpawner_;
-    #region private
 
+    #region private
+    private GameObject player_;
+    private ArrayList enemyArray_;
     #endregion
-    // Use this for initialization
+
 	void Start ()
     {
         player_ = GameObject.Find("Player");
+        SetValues();
 	}
 	
-	// Update is called once per frame
 	void Update () 
     {
-	
+        RemoveEnemies();
 	}
+
+    void SetValues()
+    {
+        //add the amount of enemies being spawned to a List
+        for(int i = 0; i < m_EnemySpawner.mEnemiesToSpawn.Length; ++i)
+        {
+            enemyArray_.Add(m_EnemySpawner.mEnemiesToSpawn[i]);
+        }
+    }
+
+    void RemoveEnemies()
+    {
+        //Loop through the spawner and if any enemy is dead remove the enemy at that index from the List of enemies 
+        for (int i = 0; i < m_EnemySpawner.mEnemiesToSpawn.Length; ++i)
+        {
+            GameObject tempBully = m_EnemySpawner.mEnemiesToSpawn[i];
+            if(!tempBully.GetComponent<BullyScript>().m_IsDead)
+            {
+                enemyArray_.RemoveAt(i);
+            }
+        }
+    }
 }
