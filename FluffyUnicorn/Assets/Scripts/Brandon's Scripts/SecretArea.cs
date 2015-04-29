9e39aac388bf50e533678d68931a4ca68b3330ed
AHBrandon <brandoncoulthard@hotmail.com>
2015-04-29 13:33:08 -0400

    More work on Secret Area

--- FluffyUnicorn/Assets/Scripts/Brandon's Scripts/SecretArea.cs
+++ FluffyUnicorn/Assets/Scripts/Brandon's Scripts/SecretArea.cs
@@ -4,7 +4,10 @@ using System.Collections;
 public class SecretArea : MonoBehaviour
 {
     #region public
-    public SpawnEnemies m_EnemySpawner;
+    public SpawnEnemies m_EnemySpawner;     //drag the spawner that is associated with the secret area into this variable
+    public int m_Row;                       //which row to spawn the enemies on 
+    public int m_EnemyType;                 //number that represents which enemy to spawn
+    public int m_NumEnemies;                //number of enemies you want to spawn 
     #endregion
 
     #region private
@@ -20,9 +23,19 @@ public class SecretArea : MonoBehaviour
 	
 	void Update () 
     {
-        RemoveEnemies();
+        //RemoveEnemies();
+        //CanUnlockArea();
 	}
 
+    void CanUnlockArea()
+    {
+        if (enemyArray_.Count == 0)
+        {
+            //unlock area
+        }
+    }
+
+    #region Add and Remove enemies
     void SetValues()
     {
         //add the amount of enemies being spawned to a List
@@ -34,14 +47,33 @@ public class SecretArea : MonoBehaviour
 
     void RemoveEnemies()
     {
-        //Loop through the spawner and if any enemy is dead remove the enemy at that index from the List of enemies 
-        for (int i = 0; i < m_EnemySpawner.mEnemiesToSpawn.Length; ++i)
+        if(m_EnemySpawner != null)
         {
-            GameObject tempBully = m_EnemySpawner.mEnemiesToSpawn[i];
-            if(!tempBully.GetComponent<BullyScript>().m_IsDead)
+            GameObject tempBully;
+            //Loop through the spawner and if any enemy is dead remove the enemy at that index from the List of enemies 
+            for (int i = 0; i < m_EnemySpawner.mEnemiesToSpawn.Length; ++i)
             {
-                enemyArray_.RemoveAt(i);
+                tempBully = m_EnemySpawner.mEnemiesToSpawn[i];
+                if (!tempBully.GetComponent<BullyScript>().m_IsDead)
+                {
+                    enemyArray_.RemoveAt(i);
+                }
+            }
+        }  
+    }
+    #endregion
+
+    #region Collision
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //when the player collides with the secret area spawn the number of enemies specified by the variable m_NumEnemies
+        if (other.tag == "Player")
+        {
+            for (int i = 0; i < m_NumEnemies; ++i)
+            {
+                m_EnemySpawner.SpawnEnemyFunc(m_Row, m_EnemyType);
             }
         }
     }
+    #endregion
 }
