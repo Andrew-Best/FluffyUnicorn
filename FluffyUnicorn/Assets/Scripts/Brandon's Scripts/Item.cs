1e85192ec662ed10686d517cda57d3c42f802021
AHBrandon <brandoncoulthard@hotmail.com>
2015-05-05 14:31:44 -0400

    Lot of treasure
    - Now with more explosions
    - Throws items out of chest and allows for you to pick what type and how
    many to spawn. When the player hits the chest or the items the
    appropriate action is taken

---
+++ FluffyUnicorn/Assets/Scripts/Brandon's Scripts/Item.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour
+{
+    #region Public variables
+    public int m_Money;     //how much money to give to the player
+
+    public string m_SpawnPoint = "Chest";
+
+    public Vector2 m_RandomXPos;
+    public Vector2 m_RandomYPos;
+    public Vector2 m_RandomXForce;
+    public Vector2 m_RandomYForce;
+    #endregion
+
+    #region Private variables
+    private PlayerController player_;
+
+    private enum m_Item
+    {
+        HEALTH = 0,
+        MONEY
+    };
+    private m_Item m_ItemType;
+    #endregion
+
+    void Start () 
+    {
+        player_ = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+	}
+
+    #region Set item
+    public void MakeHealth()
+    {
+        m_ItemType = m_Item.HEALTH;
+    }
+    public void MakeMoney()
+    {
+        m_ItemType = m_Item.MONEY;
+    }
+
+    public void SetImage(Sprite image)
+    {
+        if (this.GetComponent<SpriteRenderer>().sprite == null) // if the sprite on spriteRenderer is null then
+        {
+            this.GetComponent<SpriteRenderer>().sprite = image;
+        }
+
+    }
+    #endregion
+
+    #region Upgrades
+    public void FillHealth()
+    {
+        //give health and remove item 
+        player_.m_PlayerHealth = Constants.PLAYER_DEFAULT_MAX_HEALTH;
+        ObjectPool.Instance.PoolObject(this.gameObject);
+    }
+
+    public void AddCurrency()
+    {
+        //give currency and remove item 
+        player_.m_Currency += m_Money;
+        ObjectPool.Instance.PoolObject(this.gameObject);
+    }
+    #endregion
+
+    public void GiveItem()
+    {
+        Vector3 randomPos = new Vector3(Random.Range(m_RandomXPos.x, m_RandomXPos.y), Random.Range(m_RandomYPos.x, m_RandomYPos.y), 0);            //random pos to spawn at
+        Vector2 randomForce = new Vector2(Random.Range(m_RandomXForce.x, m_RandomXForce.y), Random.Range(m_RandomYForce.x, m_RandomYForce.y));     //random force to throw the item
+
+        GameObject spawn = GameObject.FindGameObjectWithTag(m_SpawnPoint);   //where to spawn at. Find the chest game object
+
+        //give item
+        //set the position at the spawn point with the random pos added to it 
+        //throw the object
+        if (m_ItemType == m_Item.HEALTH)
+        {
+            this.transform.position = spawn.transform.position + randomPos;
+            this.GetComponent<Rigidbody2D>().AddForce(randomForce);
+        }
+        else if (m_ItemType == m_Item.MONEY)
+        {
+            this.transform.position = spawn.transform.position + randomPos;
+            this.GetComponent<Rigidbody2D>().AddForce(randomForce);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            if (m_ItemType == m_Item.HEALTH)
+            {
+                FillHealth();
+            }
+            else if (m_ItemType == m_Item.MONEY)
+            {
+                AddCurrency();
+            }
+        }
+    }
+}
--- FluffyUnicorn/Assets/Scripts/Brandon's Scripts/Loot.cs
+++ FluffyUnicorn/Assets/Scripts/Brandon's Scripts/Loot.cs
@@ -5,24 +5,47 @@ using System;
 public class Loot : MonoBehaviour
 {
     #region Public variables
-    //change to whatever loot you want to give the player
-    public enum m_Loot
+    //item types. Add here for new items
+    public enum m_Item
     {
         HEALTH = 0,
         MONEY
     };
-    public m_Loot m_LootType;
-
-    public int m_Money;     //how much money to give to the player
-
+    public m_Item m_ItemType;
     public PlayerController m_Player;
+
+    //sound effect to be played when chest is opened 
+    public AudioClip m_SFX; 
+
+    //images for the different items
+    public Sprite m_HealthSprite;
+    public Sprite m_MoneySprite;
+
+    //animations 
+    public string m_AnimationName = "PlayFireWorks";
+    public string m_ParticleName = "Firework";
+
+    //how long you want the explosion to last when the chest is opened 
+    public float m_ParticleLifeTime = 0.5f;
+
+    //how many items you want to spawn when chest is opened 
+    public int m_NumItems = 1;
     #endregion
 
     #region Private variables
+    //only trigger chest once 
     private bool canGiveLoot_ = true;
+    //check if you can play the finished animation 
     private bool playFinishedAnimation_ = false;
+    //check if you can update particle effect
+    private bool updateParticle_ = false;
 
+    private float particleTimer_ = 0.0f;
+
+    //animators and sprite renderers
     private Animator animator_;
+    private Animator fireworkAnimator_;
+    private SpriteRenderer fireworkRenderer_;
     #endregion
 
     void Start()
@@ -33,23 +56,25 @@ public class Loot : MonoBehaviour
     void Update()
     {
         SetAnimationValues();   //update animation
+        UpdateParticleTimer();  //update particles if allowed
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        UpgradeManager upgrades = other.gameObject.GetComponentInChildren<UpgradeManager>();
         if (other.tag == "Player" && canGiveLoot_)
         {
             canGiveLoot_ = !canGiveLoot_;
-            if(m_LootType == m_Loot.HEALTH)
-            {
-                FillHealth();
-            }
-            else if(m_LootType == m_Loot.MONEY)
-            {
-                AddCurrency();
-            }
+            AudioSource.PlayClipAtPoint(m_SFX, this.transform.position);
+            PlayParticleEffect();
             playFinishedAnimation_ = true;
+            //loop through the number of items you want to spawn
+            for(int i = 0; i < m_NumItems; ++i)
+            {
+                //pool the item from the object pool and initialize and spawn the appropriate item
+                GameObject item = ObjectPool.Instance.GetObjectForType("Item", true);
+                SetItem(item.GetComponent<Item>());
+                item.GetComponent<Item>().GiveItem();
+            }            
         }
     }
 
@@ -58,15 +83,47 @@ public class Loot : MonoBehaviour
         animator_.SetBool("Finished", playFinishedAnimation_);
     }
 
-    #region Upgrades
-    public void FillHealth()
+    #region Particles
+    void PlayParticleEffect()
     {
-        m_Player.m_PlayerHealth = Constants.PLAYER_DEFAULT_MAX_HEALTH;     
+        //turn on renderers and animators and play the firework
+        fireworkAnimator_ = GameObject.FindGameObjectWithTag(m_ParticleName).GetComponent<Animator>();
+        fireworkRenderer_ = GameObject.FindGameObjectWithTag(m_ParticleName).GetComponent<SpriteRenderer>();
+        fireworkRenderer_.enabled = true;
+        fireworkAnimator_.enabled = true;
+        fireworkAnimator_.Play(m_AnimationName);
+        updateParticle_ = true;      
     }
 
-    public void AddCurrency()
+    void UpdateParticleTimer()
     {
-        m_Player.m_Currency += m_Money;  
+        //if the particle is playing increment a timer until it reaches the max time and then stop
+        if (updateParticle_)
+        {
+            particleTimer_ += Time.deltaTime;
+        }
+        if(particleTimer_ >= m_ParticleLifeTime)
+        {
+            updateParticle_ = false;
+            particleTimer_ = 0.0f;
+            fireworkRenderer_.enabled = false;
+            fireworkAnimator_.enabled = false;
+        }
     }
     #endregion
+
+    void SetItem(Item item)
+    {
+        //set the item based on what enum you picked in the inspector
+        if(m_ItemType == m_Item.HEALTH)
+        {
+            item.MakeHealth();
+            item.SetImage(m_HealthSprite);
+        }
+        else if(m_ItemType == m_Item.MONEY)
+        {
+            item.MakeMoney();
+            item.SetImage(m_MoneySprite);
+        }
+    }
 }
