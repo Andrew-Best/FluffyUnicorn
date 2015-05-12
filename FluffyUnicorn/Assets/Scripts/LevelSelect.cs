231f23ad0321258de5a1bbd8669f237ef60eb481
Andrew Best <andrew.c.best@gmail.com>
2015-05-12 16:38:49 -0400

    Level Select, Main Menu, UI images, oh my!

---
+++ FluffyUnicorn/Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSelect : MonoBehaviour
+{
+    public void ChooseLevel(GameObject go)
+    {
+        //Debug.Log(go.name);
+        Application.LoadLevel(go.name);
+    }
+}
---
+++ FluffyUnicorn/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class MainMenuScript : MonoBehaviour 
+{
+    public Button m_ContinueButton;
+
+	void Start ()
+    {
+        if (!File.Exists(Application.persistentDataPath + Path.DirectorySeparatorChar + "fuSaveData.dat"))
+        {
+            m_ContinueButton.interactable = true;
+        }
+	}
+	
+	void Update ()
+    {
+	
+	}
+}
