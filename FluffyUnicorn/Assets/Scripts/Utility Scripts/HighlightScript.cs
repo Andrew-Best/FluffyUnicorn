be0cbef55e3ccd9e74d0cbd030c285a4f2ff824d
Andrew Best <andrew.c.best@gmail.com>
2015-05-14 15:54:58 -0400

    Start of Highlight Script
    It's to highlight portions of the area rather than cheating with buttons
    like I had previously.

---
+++ FluffyUnicorn/Assets/Scripts/Utility Scripts/HighlightScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HighlightScript : MonoBehaviour 
+{
+    private Image buttonImage_;
+
+    private int alphaMin_ = 0;          //Lowest number the alpha can go to
+    private int alphaMax_ = 134;        //Highest number the alpha can go to
+
+    private float alphaTimer_;          //Timer for the alpha interpolation
+    private float timerMax_ = 10.0f;     //Max time for the alphaTimer_
+    private float timerMin_ = 0.0f;     //Min time for the alphaTimer_
+    private float lerpSpeed_ = 0.3f;
+    private float tParam_ = 0.0f;
+
+    private bool switchAlpha_ = false;  //Bool to determine whether or not the alpha should be increasing or decreasing. False = increase, True = decrease
+	
+    void Start()
+    {
+        buttonImage_ = this.GetComponent<Image>();
+    }
+
+	void Update ()
+    {
+        alphaTimer_ += Time.deltaTime;
+
+
+        if(alphaTimer_ >= timerMax_)
+        {
+            Debug.Log(switchAlpha_);
+            switchAlpha_ = !switchAlpha_;
+            alphaTimer_ = timerMin_;
+        }
+
+        if (!switchAlpha_)
+        {
+            Debug.Log("Increase Alpha");
+            buttonImage_.CrossFadeAlpha(alphaMax_, 200.0f, false);
+            Debug.Log(buttonImage_.color.a);
+        }
+        else if(switchAlpha_)
+        {
+            Debug.Log("Decrease Alpha");
+            buttonImage_.CrossFadeAlpha(0, 200.0f, false);
+            Debug.Log(buttonImage_.color.a);
+        }
+	}
+}
