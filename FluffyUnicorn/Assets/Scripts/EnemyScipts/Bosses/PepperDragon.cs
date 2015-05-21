2efc8bab56029c45e87fcc70b04e0f410d92ac87
Adlez <srgnt_booyaw@live.ca>
2015-05-21 14:06:07 -0400

    PepperDragon v.0.3
    -PepperDragon Collision nearing completeness

--- FluffyUnicorn/Assets/Scripts/EnemyScipts/Bosses/PepperDragon.cs
+++ FluffyUnicorn/Assets/Scripts/EnemyScipts/Bosses/PepperDragon.cs
@@ -9,6 +9,8 @@ public class PepperDragon : BossBaseClass
 
 	public GameObject m_PepperDragon;
 
+	public string m_PlayerLayer;
+
 	public const float DRAGON_PART_MOVEMENT = 0.5f;
 
 	public void SwitchRow(GameObject head, GameObject hitArm)
@@ -16,8 +18,35 @@ public class PepperDragon : BossBaseClass
 		head.GetComponent<PepperDragonHead>().m_ThisHeadStartYPos = m_Head.GetComponent<Rigidbody2D>().transform.position.y;
 		hitArm.GetComponent<PepperDragonArm>().m_ThisArmStartPosY = hitArm.GetComponent<Rigidbody2D>().transform.position.y;
 
-		head.GetComponent<PepperDragonHead>().MoveToArmPos(hitArm);
-		hitArm.GetComponent<PepperDragonArm>().MoveToHeadPos();
+		string headStartLayer = head.gameObject.layer.ToString();
+		string armStartLayer = hitArm.gameObject.layer.ToString();
+
+		head.GetComponent<PepperDragonHead>().MoveToArmPos(hitArm, armStartLayer, headStartLayer);
+		hitArm.GetComponent<PepperDragonArm>().MoveToHeadPos(armStartLayer, headStartLayer);
+	}
+
+	protected bool MatchPlayerRowToLayer(string LayerOfEnemy)
+	{
+		if (this.GetComponent<EnemyBaseClass>().m_PlayerCurRow == 0)
+		{
+			m_PlayerLayer = "PDBackRow";
+		}
+		else if (this.GetComponent<EnemyBaseClass>().m_PlayerCurRow == 1)
+		{
+			m_PlayerLayer = "PDMidRow";
+		}
+		else if (this.GetComponent<EnemyBaseClass>().m_PlayerCurRow == 2)
+		{
+			m_PlayerLayer = "PDFrontRow";
+		}
+		if(LayerOfEnemy == m_PlayerLayer)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 
 	private void SpewPepper()
--- FluffyUnicorn/Assets/Scripts/EnemyScipts/Bosses/PepperDragonArm.cs
+++ FluffyUnicorn/Assets/Scripts/EnemyScipts/Bosses/PepperDragonArm.cs
@@ -7,47 +7,61 @@ public class PepperDragonArm : PepperDragon
 	public float m_ThisArmDestPosY;//AKA the start Y of the head
 	public float m_ThisArmStartPosY;
 
+	bool ArmIsMoving_;
+
 	public GameObject m_DragonHead;
 
-	public void MoveToHeadPos()
-	{
-		if(m_ThisArmStartPosY > m_DragonHead.GetComponent<PepperDragonHead>().m_ThisHeadStartYPos)//the arm is higher and must be lowered
-		{
-			if (m_ThisArmCurPosY > m_DragonHead.GetComponent<PepperDragonHead>().m_ThisHeadStartYPos)
-			{
-				this.GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, -1.0f);
-			}			
-		}
-		else//the arm is lower and must be raised
-		{
-			if(m_ThisArmCurPosY < m_DragonHead.GetComponent<PepperDragonHead>().m_ThisHeadStartYPos)
-			{
-				this.GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, 1.0f);
-			}
-		}
+	public void MoveToHeadPos(string armLayer, string headLayer)
+	{		
+		ArmIsMoving_ = true;
+		this.gameObject.layer = LayerMask.NameToLayer(headLayer);
 	}
 
 	void OnTriggerEnter2D(Collider2D playerAttack)
 	{
 		if (playerAttack.tag == "PlayerProjectile")
 		{
-			m_PepperDragon.GetComponent<BossBaseClass>().m_HP -= playerAttack.GetComponent<Projectile>().m_Damage;
-			m_PepperDragon.GetComponent<PepperDragon>().SwitchRow(m_Head, this.gameObject);
+			string ass = this.gameObject.layer.ToString();
+			int assValue = int.Parse(ass);
+
+			string curLayerName = LayerMask.LayerToName(assValue);
+		
+			if (MatchPlayerRowToLayer(curLayerName))
+			{
+				
+				m_PepperDragon.GetComponent<BossBaseClass>().m_HP -= playerAttack.GetComponent<Projectile>().m_Damage;
+				m_PepperDragon.GetComponent<PepperDragon>().SwitchRow(m_Head, this.gameObject);
+			}
 		}
 	}
 
 	// Use this for initialization
 	void Start()
 	{
-
+		ArmIsMoving_ = false;
 	}
-
-
 
 	// Update is called once per frame
 	void Update()
 	{
 		m_ThisArmCurPosY = this.GetComponent<Rigidbody2D>().transform.position.y;
+		if(ArmIsMoving_)
+		{
+			if (m_ThisArmStartPosY > m_DragonHead.GetComponent<PepperDragonHead>().m_ThisHeadStartYPos)//the arm is higher and must be lowered
+			{
+				if (m_ThisArmCurPosY > m_DragonHead.GetComponent<PepperDragonHead>().m_ThisHeadStartYPos)
+				{
+					this.GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, -1.0f);
+				}
+			}
+			else//the arm is lower and must be raised
+			{
+				if (m_ThisArmCurPosY < m_DragonHead.GetComponent<PepperDragonHead>().m_ThisHeadStartYPos)
+				{
+					this.GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, 1.0f);
+				}
+			}
+		}
 	}
 
 }
