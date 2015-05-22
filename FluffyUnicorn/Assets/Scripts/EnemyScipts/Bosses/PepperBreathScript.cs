8641d8acdb40d7b1b1270719efb9e585d020a497
Adlez <srgnt_booyaw@live.ca>
2015-05-22 15:21:20 -0400

    PepperDragon v1.0
    Breath and Pigtail particles started

---
+++ FluffyUnicorn/Assets/Scripts/EnemyScipts/Bosses/PepperBreathScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PepperBreathScript : MonoBehaviour 
+{
+	public GameObject m_PepperBreath;
+	public ParticleSystem m_PepperParticles;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
