a08f1ee6f8e1c7e5b08dec948e8ca9cf49915fed
BrendanM <danmatrix@live.com>
2015-05-25 14:42:52 -0400

    Bullies are on the actual rows now

--- FluffyUnicorn/Assets/Scripts/EnemyScipts/SpawnEnemies.cs
+++ FluffyUnicorn/Assets/Scripts/EnemyScipts/SpawnEnemies.cs
@@ -13,29 +13,48 @@ public class SpawnEnemies : MonoBehaviour
 
     public static bool bossSpawned_ = false;
 
+    private Vector2 startPos_;
+    private Vector2 xOffSet_;
+
 	void Start () 
 	{
 		m_EnemyControl = GameObject.FindGameObjectWithTag("EnemyController"); 
 		mSpawnPos[0] = GameObject.FindGameObjectWithTag("ESRL");
 		mSpawnPos[1] = GameObject.FindGameObjectWithTag("ESRM");
 		mSpawnPos[2] = GameObject.FindGameObjectWithTag("ESRF");
+        m_Tracks = GameObject.FindGameObjectWithTag("EnemyController").GetComponent<EnemyBaseClass>().m_TargetPoints;
 		if(mSpawnPos.Length == 0)
 		{
 			Debug.LogError("Spawn Area needs spawn positions.");
 		}
+        xOffSet_ = new Vector2(11.0f, 0.0f);
 	}
 	
 	public void SpawnEnemyFunc(int row, int type)
 	{
+        int startRow = Random.Range(0, 2);
+        if (startRow == 0)
+        {
+            startPos_ = m_Tracks[0].transform.position;
+        }
+        if (startRow == 1)
+        {
+            startPos_ = m_Tracks[1].transform.position;
+        }
+        if (startRow == 2)
+        {
+            startPos_ = m_Tracks[2].transform.position;
+        }
+
 		GameObject newEnemy = Objectpooler.Instance.GetObjectForType(mEnemiesToSpawn[type].name, true);//new enemy is created
-		newEnemy.transform.position = mSpawnPos[row].transform.position; //the enemy's position is assigned the position at the selected row
+        newEnemy.transform.position = startPos_ + xOffSet_; //mSpawnPos[row].transform.position; //the enemy's position is assigned the position at the selected row
 
 		m_EnemyControl.GetComponent<EnemyControllerScript>().AddBullyToList(newEnemy);
-		newEnemy.GetComponent<BullyScript>().InitEnemy(mSpawnPos[row].transform.position, row, newEnemy);
+        newEnemy.GetComponent<BullyScript>().InitEnemy(startPos_ + xOffSet_, row, newEnemy);
 
 	}
 
-	public void SpawnBoss(int row, string BossName)
+    public void SpawnBoss(int row, string BossName)
 	{
 		for(int i = 0; i < mEnemiesToSpawn.Length; ++i)
 		{
