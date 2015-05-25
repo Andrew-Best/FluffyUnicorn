bf33477960f71add0b648c8ef7666d5cd2d731f0
Adlez <srgnt_booyaw@live.ca>
2015-05-25 13:56:47 -0400

    PepperDragon v1.1
    SmackAttack just needs to stop on the way down

--- FluffyUnicorn/Assets/Scripts/EnemyScipts/Bosses/PepperDragonArm.cs
+++ FluffyUnicorn/Assets/Scripts/EnemyScipts/Bosses/PepperDragonArm.cs
@@ -7,14 +7,31 @@ public class PepperDragonArm : PepperDragon
 	public float m_ThisArmDestPosY;//AKA the start Y of the head
 	public float m_ThisArmStartPosY;
 
+	private float timeUntilHandRises_; //time till hand rises
+	private float raiseHandTimer_;
+	private bool HandRaising_ = false;
+
 	bool ArmIsMoving_;
 
 	public GameObject m_DragonHead;
+
 
 	public void MoveToHeadPos(int armLayerIndex, int headLayerIndex)
 	{		
 		ArmIsMoving_ = true;
 		this.gameObject.layer =headLayerIndex;
+	}
+
+	public void ReadyTheSlaps()
+	{
+		this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(Constants.RAISE_HAND_VELX, Constants.RAISE_HAND_VELY) ;
+		HandRaising_ = true;
+	}
+
+	public void SmackDown()
+	{
+		this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(Constants.SMACK_DOWN_VELX, Constants.SMACK_DOWN_VELY);
+		timeUntilHandRises_ = Random.Range(Constants.MIN_TIME_UNTIL_SLAP, Constants.MAX_TIME_UNTIL_SLAP);
 	}
 
 	void OnTriggerEnter2D(Collider2D playerAttack)
@@ -36,12 +53,32 @@ public class PepperDragonArm : PepperDragon
 	{
 		ArmIsMoving_ = false;
 		m_Player = GameObject.FindGameObjectWithTag("Player");
+		timeUntilHandRises_ = Random.Range(Constants.MIN_TIME_UNTIL_SLAP, Constants.MAX_TIME_UNTIL_SLAP);
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
+		if (!HandRaising_)//if the hand is not raising (i.e if falling down or not moving)
+		{
+			timeUntilHandRises_ -= Time.deltaTime;//countdown until the next time the hand is raised
+			if (timeUntilHandRises_ <= 0)
+			{
+				timeUntilHandRises_ = 0;//set timer to 0
+				ReadyTheSlaps();//function that raises the hand. Changes the HandRaising bool to true
+			}
+		}
 
+		if(HandRaising_)//After ReadyTheSlaps is called, If the hand is raising into the air
+		{			
+			raiseHandTimer_ += Time.deltaTime;//count up until the hand SMACKS down
+			if (raiseHandTimer_ >= 6)
+			{
+				raiseHandTimer_ = 0;//set the timer to 0
+				SmackDown();//Slam the hand into the ground
+				HandRaising_ = false;//Change the boolean to clarify the hand should now be smacking the ground
+			}
+		}
 		m_ThisArmCurPosY = this.GetComponent<Rigidbody2D>().transform.position.y;
 		if(ArmIsMoving_)
 		{
@@ -72,6 +109,22 @@ public class PepperDragonArm : PepperDragon
 			ChangeZPos();
 		}
 		GetCurLayer();
+	}
+
+	void OnCollisionEnter2D(Collision2D ground)
+	{
+		if(ground.gameObject.tag == "Track0" && this.gameObject.layer == layerIndex)
+		{
+			this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+		}
+		else if (ground.gameObject.tag == "Track1" && this.gameObject.layer == layerIndex)
+		{
+			this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+		}
+		else if (ground.gameObject.tag == "Track2" && this.gameObject.layer == layerIndex)
+		{
+			this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+		}
 	}
 
 }
