bd76af0c13f4288597191bb2113657a2bd93c6ed
BrendanM <danmatrix@live.com>
2015-05-26 19:24:11 -0400

    Jocky Bully Update
    Getting there

---
+++ FluffyUnicorn/Assets/Scripts/EnemyScipts/BullyCountdown.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BullyCountdown : MonoBehaviour 
+{
+    public GameObject m_Jocky;
+
+    private float Activate_;
+
+	
+	void Start () 
+    {
+        Activate_ = 10.0f;
+        
+	}
+	
+	
+	void Update ()
+    {
+        Activate_ -= Time.deltaTime;
+        if(Activate_ <= 0.0f)
+        {
+            m_Jocky.SetActive(true);
+        }
+	}
+}
--- FluffyUnicorn/Assets/Scripts/EnemyScipts/HorsebackBully.cs
+++ FluffyUnicorn/Assets/Scripts/EnemyScipts/HorsebackBully.cs
@@ -11,16 +11,19 @@ public class HorsebackBully : MonoBehaviour
 
     private List<GameObject> Horses_ = new List<GameObject>();
 
-    private int MaxSpawn_ = 1;
+    private int MaxSpawn_;
 
-    private float Respawn_ = 10.0f;
-    private float Reset_ = 5.0f;
+    private float Respawn_;
+    private float Reset_;
+    private float Vel_;
 
     private Rigidbody2D Velocity_;
 
-    private Vector2 Pos_;
+    private Vector2 XOffset_;
+    private Vector2 StartPos_;
 
-    private bool FirstSpawn_ = true;
+    private bool FirstSpawn_;
+    private bool Turn_;
 	
 	void Start () 
     {
@@ -28,25 +31,36 @@ public class HorsebackBully : MonoBehaviour
         EnemySpawner_ = GameObject.FindGameObjectWithTag("EnemySpawner").GetComponent<SpawnEnemies>();
         Velocity_ = gameObject.GetComponent<Rigidbody2D>();
         Bully_ = gameObject.GetComponent<BullyScript>();
+
+        Respawn_ = 10.0f;
+        Reset_ = 10.0f;
+        MaxSpawn_ = 1;
+        FirstSpawn_ = true;
+        Turn_ = false;
+        //XOffset_ = new Vector2(11.0f, 0.0f);
+        StartPos_ = new Vector2(0.0f, 0.0f);
+
 	}
 	
 	void Update () 
     {
         Respawn_ -= Time.deltaTime;
+        //Debug.Log(Respawn_);
         Reset_ -= Time.deltaTime;
-
-        if (FirstSpawn_ == true)
+        if(FirstSpawn_ == true)
         {
-            SetJockys();
-            Debug.Log("Set!");
+            SetMoreJockys();
             FirstSpawn_ = false;
         }
-        FirstSpawn_ = false;
-
         if(Respawn_ <= 0.0f)
         {
+            Turn_ = true;
             RecycleJockys();
             Respawn_ = 10.0f;
+        }
+        else
+        {
+            Turn_ = false;
         }
         if(Reset_ <= 0.0f)
         {
@@ -56,28 +70,36 @@ public class HorsebackBully : MonoBehaviour
             }
         }
 
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, 0.0f);
+
         for(int i = 0; i < Horses_.Count; ++i)
         {
-            Velocity_.velocity = new Vector2(3.0f, 0.0f);
+            Vel_ = Random.Range(0.0f, -5.0f);
+            Horses_[i].GetComponent<Rigidbody2D>().velocity = new Vector2(Vel_, 0.0f);
         }
+
+        
 	
 	}
 
-    void SetJockys()
+    void SetMoreJockys()
     {
+        gameObject.SetActive(true);
         int row = 0;
-        Vector2 StartPos = new Vector2(11.0f, 0.0f);
+        Vector2 StartPos = new Vector2(9.0f, 0.0f);
         for (int i = 0; i < EnemySpawner_.mEnemiesToSpawn.Length; ++i)
         {
             string bullyName = EnemySpawner_.mEnemiesToSpawn[i].name;
             if (bullyName == "Jocky Bully")
             {
+                MaxSpawn_ = Random.Range(1, 2);
                 for (int j = 0; j < MaxSpawn_; ++j)
                 {
-                    newEnemy = Objectpooler.Instance.GetObjectForType(bullyName, true);//new enemy is created
-                    newEnemy.transform.position = StartPos;
-                    Horses_.Add(newEnemy);
-                    //newEnemy.GetComponent<BullyScript>().InitEnemy(StartPos, row, newEnemy);
+                    Objectpooler.Instance.GetObjectForType(bullyName, true);//new enemy is created
+                    gameObject.transform.position = StartPos;
+                    Horses_.Add(gameObject);
+                    gameObject.GetComponent<BullyScript>().InitEnemy(StartPos, row, gameObject);
+                    
                 }
 
             }
@@ -86,24 +108,29 @@ public class HorsebackBully : MonoBehaviour
 
     void RecycleJockys()
     {
-        int startPos = Random.Range(0, 2);
         int row = 0;
+        int startPos = Random.Range(0, 2);
         if(startPos == 0)
         {
-            row = 0;
+            StartPos_.y = Bully_.m_TargetPoints[0].transform.position.y;
         }
         if(startPos == 1)
         {
-            row = 1;
+            StartPos_.y = Bully_.m_TargetPoints[1].transform.position.y;
         }
         if (startPos == 2)
         {
-            row = 2;
+            StartPos_.y = Bully_.m_TargetPoints[2].transform.position.y;
         }
+
+        float dist = Random.Range(11.0f, 15.0f);
+        XOffset_ = new Vector2(dist, 0.0f);
 
         for (int j = 0; j < Horses_.Count; ++j)
         {
-            Horses_[j].transform.position = new Vector2(11.0f, Bully_.m_TargetPoints[row].transform.position.y);
+            Horses_[j].transform.position = StartPos_ + XOffset_;
+            Horses_[j].GetComponent<BullyScript>().InitEnemy(StartPos_ + XOffset_, row, gameObject);
+            
         }
     }
 }
