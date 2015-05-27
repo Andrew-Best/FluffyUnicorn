27bc5a75adc7700612704130a7c3809858c56f70
BrendanM <danmatrix@live.com>
2015-05-27 18:12:54 -0400

    Jocky Bully update
    Added the unique attack which is not working as inteneded yet :/

--- FluffyUnicorn/Assets/Scripts/EnemyScipts/HorsebackBully.cs
+++ FluffyUnicorn/Assets/Scripts/EnemyScipts/HorsebackBully.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class HorsebackBully : MonoBehaviour
 {
     private GameObject Player_;
+    private PlayerController PC_;
     private SpawnEnemies EnemySpawner_;
     private GameObject newEnemy;
     private BullyScript Bully_;
@@ -12,6 +13,7 @@ public class HorsebackBully : MonoBehaviour
     private List<GameObject> Horses_ = new List<GameObject>();
 
     private int MaxSpawn_;
+    private int currRow_;
 
     private float Respawn_;
     private float Reset_;
@@ -19,15 +21,19 @@ public class HorsebackBully : MonoBehaviour
 
     private Rigidbody2D Velocity_;
 
-    private Vector2 XOffset_;
-    private Vector2 StartPos_;
+    private Vector3 XOffset_;
+    private Vector3 StartPos_;
 
     private bool FirstSpawn_;
     private bool Turn_;
+    private bool FrontOccupied_;
+    private bool MiddleOccupied_;
+    private bool BackOccupied_;
 	
 	void Start () 
     {
         Player_ = GameObject.FindGameObjectWithTag("Player");
+        PC_ = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
         EnemySpawner_ = GameObject.FindGameObjectWithTag("EnemySpawner").GetComponent<SpawnEnemies>();
         Velocity_ = gameObject.GetComponent<Rigidbody2D>();
         Bully_ = gameObject.GetComponent<BullyScript>();
@@ -37,8 +43,11 @@ public class HorsebackBully : MonoBehaviour
         MaxSpawn_ = 1;
         FirstSpawn_ = true;
         Turn_ = false;
-        //XOffset_ = new Vector2(11.0f, 0.0f);
-        StartPos_ = new Vector2(0.0f, 0.0f);
+        XOffset_ = new Vector3(0.0f, 0.0f, 0.0f);
+        StartPos_ = new Vector3(0.0f, 0.0f, 0.0f);
+        FrontOccupied_ = false;
+        MiddleOccupied_ = false;
+        BackOccupied_ = false;
 
 	}
 	
@@ -74,10 +83,10 @@ public class HorsebackBully : MonoBehaviour
 
         for(int i = 0; i < Horses_.Count; ++i)
         {
-            Vel_ = Random.Range(0.0f, -5.0f);
+            Vel_ = Random.Range(-2.0f, -6.0f);
             Horses_[i].GetComponent<Rigidbody2D>().velocity = new Vector2(Vel_, 0.0f);
         }
-
+       
         
 	
 	}
@@ -109,22 +118,32 @@ public class HorsebackBully : MonoBehaviour
     void RecycleJockys()
     {
         int row = 0;
-        int startPos = Random.Range(0, 2);
+        int startPos = Random.Range(0, 3);
         if(startPos == 0)
         {
             StartPos_.y = Bully_.m_TargetPoints[0].transform.position.y;
+            StartPos_.z = Bully_.m_TargetPoints[0].transform.position.z;
+            currRow_ = 0;
+            Debug.Log(0);
         }
         if(startPos == 1)
         {
             StartPos_.y = Bully_.m_TargetPoints[1].transform.position.y;
+            StartPos_.z = Bully_.m_TargetPoints[1].transform.position.z;
+            currRow_ = 1;
+            Debug.Log(1);
+            MiddleOccupied_ = true;
         }
         if (startPos == 2)
         {
             StartPos_.y = Bully_.m_TargetPoints[2].transform.position.y;
+            StartPos_.z = Bully_.m_TargetPoints[2].transform.position.z;
+            currRow_ = 2;
+            Debug.Log(2);
         }
 
-        float dist = Random.Range(11.0f, 15.0f);
-        XOffset_ = new Vector2(dist, 0.0f);
+        float dist = Random.Range(10.0f, 15.0f);
+        XOffset_ = new Vector3(dist, 0.0f, 0.0f);
 
         for (int j = 0; j < Horses_.Count; ++j)
         {
@@ -133,4 +152,61 @@ public class HorsebackBully : MonoBehaviour
             
         }
     }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" && PC_.m_onMiddleTrack == true)
+        {
+            Player_.transform.position = new Vector3(Player_.transform.position.x, Bully_.m_TargetPoints[2].transform.position.y, Bully_.m_TargetPoints[2].transform.position.z);
+            PC_.m_onFrontTrack = true;
+            PC_.m_onMiddleTrack = false;
+            //Physics2D.IgnoreLayerCollision(0, 18, true);
+        }
+
+        if (collision.tag == "Player" && PC_.m_onFrontTrack == true)
+        {
+            Player_.transform.position = new Vector3(Player_.transform.position.x, Bully_.m_TargetPoints[1].transform.position.y, Bully_.m_TargetPoints[1].transform.position.z);
+            PC_.m_onMiddleTrack = true;
+            PC_.m_onFrontTrack = false;
+            //Physics2D.IgnoreLayerCollision(0, 18, true);
+        }
+
+        if (collision.tag == "Player" && PC_.m_onLastTrack == true)
+        {
+            Player_.transform.position = new Vector3(Player_.transform.position.x, Bully_.m_TargetPoints[1].transform.position.y, Bully_.m_TargetPoints[1].transform.position.z);
+            PC_.m_onMiddleTrack = true;
+            PC_.m_onLastTrack = false;
+            //Physics2D.IgnoreLayerCollision(0, 18, true);
+        }
+            /*if(PC_.m_onMiddleTrack == true)
+            {
+                Player_.transform.position = new Vector2(Player_.transform.position.x, Bully_.m_TargetPoints[0].transform.position.y);
+                PC_.m_onFrontTrack = true;
+                Physics2D.IgnoreLayerCollision(0, 18, true);
+            }
+
+            if(PC_.m_onFrontTrack == true)
+            {
+                Player_.transform.position = new Vector2(Player_.transform.position.x, Bully_.m_TargetPoints[1].transform.position.y);
+                PC_.m_onMiddleTrack = true;
+                Physics2D.IgnoreLayerCollision(0, 18, true);
+            }
+
+            if(PC_.m_onLastTrack == true)
+            {
+                Player_.transform.position = new Vector2(Player_.transform.position.x, Bully_.m_TargetPoints[1].transform.position.y);
+                PC_.m_onMiddleTrack = true;
+                Physics2D.IgnoreLayerCollision(0, 18, true);
+            }*/
+        //}
+       
+    }
+
+    /*void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            Physics2D.IgnoreLayerCollision(0, 18, false);
+        }
+    }*/
 }
