72d4701873211894fd853646cde2171abbc9d4d4
AHBrandon <brandoncoulthard@hotmail.com>
2015-06-01 11:16:28 -0400

    Cutscene test

--- FluffyUnicorn/Assets/Scripts/Brandon's Scripts/Cutscene.cs
+++ FluffyUnicorn/Assets/Scripts/Brandon's Scripts/Cutscene.cs
@@ -1,15 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
-public class Cutscene : MonoBehaviour {
+public class Cutscene : MonoBehaviour 
+{
+    public string m_SceneName;
+    public List<GameObject> m_Scenes;
+    private int sceneCounter_ = 0;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    //changes background as well as message
+    public void NextScene()
+    {
+        //set the previous pannel off and then set the next one on  
+        m_Scenes[sceneCounter_].SetActive(false);
+        sceneCounter_++;
+        if(sceneCounter_ < m_Scenes.Count)
+        {
+            m_Scenes[sceneCounter_].SetActive(true);
+        }
+        else
+        {
+            Application.LoadLevel(m_SceneName);
+        }
+    }
 }
